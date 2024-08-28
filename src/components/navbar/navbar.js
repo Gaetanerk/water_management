1e3344f2195a4215a22187335a5eb2b792e7eee3
Gaetanerk <gaetan.turben@outlook.com>
2024-08-28 16:07:46 +0200

    navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,28 @@
+import Foyer from './foyer';
+import Recommandations from './recommandations';
+import Assistance from './assistance';
+import './navbar.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <Router>
+        <nav>
+            <ul id='ulNavbar'>
+                <img id='logo' src="./images/navbar/Logo_Lille_mais_bleu.svg"></img>
+                <li><Link to="/foyer" className="nav-link">Foyer</Link></li>
+                <li><Link to="/recommandations" className="nav-link">Recommandations</Link></li>
+                <li><Link to="/assistance" className="nav-link">Assistance</Link></li>
+            </ul>
+            <div id='lignBlue'></div>
+        </nav>
+        <Routes>
+            <Route path="/foyer" element={<Foyer />} />
+            <Route path="/recommandations" element={<Recommandations />} />
+            <Route path="/assistance" element={<Assistance />} />
+        </Routes>
+    </Router>
+  );
+};
+
+export default Navbar;
