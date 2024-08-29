61c9bcb3a7a32e89ab2f0cbfb6954ff9589a75f7
Gaetanerk <gaetan.turben@outlook.com>
2024-08-29 11:08:23 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
-=======
-import FormulaireConnexion from './components/formulaire_connexion/formulaire';
-
-function App() {
-  return (
-    <div className="App">
-      <FormulaireConnexion />
->>>>>>> formulaire_connexion
     </div>
   );
 }
