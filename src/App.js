f74efec03ffb29f1c44b1ca49dde40807b9f120f
Gaetanerk <gaetan.turben@outlook.com>
2024-08-29 12:13:27 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import Navbar from './components/navbar/navbar';
-=======
-import Alertes from './components/alertes_recentes/Alertes';
->>>>>>> alerteRecente
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <Navbar />
-=======
-      <Alertes />
->>>>>>> alerteRecente
     </div>
   );
 }
