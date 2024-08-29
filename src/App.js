938b477d99e3a93594a7a4235fb796e5086c99fc
Gaetanerk <gaetan.turben@outlook.com>
2024-08-29 13:58:28 +0200

    conso24h

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Conso24h from './components/conso24h/conso24h';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Conso24h />
     </div>
   );
 }
