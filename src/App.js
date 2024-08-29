508148b5cabd7b27258eb49b2ab0473e843fe712
Gaetanerk <gaetan.turben@outlook.com>
2024-08-29 13:42:24 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
-=======
-import Recommandations from './components/recommandations/recommandations';
-
-function App() {
-  return (
-    <div className="App">
-      <Recommandations />
->>>>>>> recommandations
     </div>
   );
 }
