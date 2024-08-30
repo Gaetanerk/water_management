75268b6ae3fe37c8c065cd6527318db6f00eab74
Gaetanerk <gaetan.turben@outlook.com>
2024-08-30 09:22:29 +0200

    navbar_mel

---
+++ src/components/navbar_mel/navbar_mel.js
@@ -0,0 +1,28 @@
+import MesEspaces from './mes_espaces';
+import Recommandations from './recommandations';
+import Assistance from './assistance';
+import './navbar.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+const NavbarMel = () => {
+  return (
+    <Router>
+        <nav>
+            <ul id='ulNavbar'>
+                <img id='logo' src="./images/navbar/Logo_Lille_mais_bleu.svg"></img>
+                <li><Link to="/mes_espaces" className="nav-link">Mes Espaces</Link></li>
+                <li><Link to="/recommandations" className="nav-link">Recommandations</Link></li>
+                <li><Link to="/assistance" className="nav-link">Assistance</Link></li>
+            </ul>
+            <div id='lignBlue'></div>
+        </nav>
+        <Routes>
+            <Route path="/mes_espaces" element={<MesEspaces />} />
+            <Route path="/recommandations" element={<Recommandations />} />
+            <Route path="/assistance" element={<Assistance />} />
+        </Routes>
+    </Router>
+  );
+};
+
+export default NavbarMel;
