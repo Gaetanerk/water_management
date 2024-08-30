a43a5abbb26b3c512b8dc9a4a735687f4c129286
Gaetanerk <gaetan.turben@outlook.com>
2024-08-30 13:48:27 +0200

    liens navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -10,17 +10,12 @@ const Navbar = () => {
       <nav>
         <ul id='ulNavbar'>
           <img id='logo' src="./images/navbar/Logo_Lille_mais_bleu.svg" alt="Logo"></img>
-          <li><Link to="/foyer" className="nav-link">Foyer</Link></li>
-          <li><Link to="/recommandations" className="nav-link">Recommandations</Link></li>
+          <li><Link to="/accueil_foyer" className="nav-link">Foyer</Link></li>
+          <li><Link to="/accueil_recommandations_utilisateur" className="nav-link">Recommandations</Link></li>
           <li><Link to="/assistance" className="nav-link">Assistance</Link></li>
         </ul>
         <div id='lignBlue'></div>
       </nav>
-      <Routes>
-        <Route path="/foyer" element={<Foyer />} />
-        <Route path="/recommandations" element={<Recommandations />} />
-        <Route path="/assistance" element={<Assistance />} />
-      </Routes>
     </div>
   );
 };
