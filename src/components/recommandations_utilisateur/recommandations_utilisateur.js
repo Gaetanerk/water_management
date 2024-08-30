9b0d7aeaf5772063064f78f73aa6c7fe6a06a557
Gaetanerk <gaetan.turben@outlook.com>
2024-08-30 10:02:42 +0200

    recommandations_utilisateur

---
+++ src/components/recommandations_utilisateur/recommandations_utilisateur.js
@@ -0,0 +1,24 @@
+import NavbarMel from '../navbar/navbar';
+import './recommandations_utilisateur.css';
+
+const RecommandationsUtilisateur = () => {
+  return (
+    <div id="recommandations_utilisateur">
+      <Navbar />
+      <h1 id='titre'>Recommandations</h1>
+      <div id='elements'>
+        <div>
+            <p>Lave Vaisselle</p>
+        </div>
+        <div>
+            <p>Lave Linge</p>
+        </div>
+        <div>
+            <p>Douche</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecommandationsUtilisateur;
