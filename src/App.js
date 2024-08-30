c92cb758798ff2ed3fcc84c35e96afe01684e754
Gaetanerk <gaetan.turben@outlook.com>
2024-08-30 11:28:28 +0200

    presentation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Presentation from './components/presentation/presentation';
+import Accueil from './components/accueil/accueil'; // Vérifiez les chemins corrects
+import AccueilMel from './components/accueil_mel/accueil_mel';
 import AccueilRecommandationsUtilisateur from './components/accueil_recommandations_utilisateur/accueil_recommandations_utilisateur';
+import Login from './components/accueil_login/accueil_login';
 
 function App() {
   return (
-    <div>
-      <AccueilRecommandationsUtilisateur />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Presentation />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/accueil" element={<Accueil />} />
+        <Route path="/accueil_mel" element={<AccueilMel />} />
+        <Route path="/accueil_recommandations_utilisateur" element={<AccueilRecommandationsUtilisateur />} />
+      </Routes>
+    </Router>
   );
 }
 
